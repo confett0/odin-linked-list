8e6ddaeca5a4a6db276d4848d4828c0d6e201ef2
confett0 <118047961+confett0@users.noreply.github.com>
2023-09-14 18:46:31 +0200

    Add append method

--- linked-list.js
+++ linked-list.js
@@ -2,6 +2,16 @@ class LinkedList {
     constructor(head = null) {
         this.head = head;
     }
+
+    append(value) {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        lastNode.next = new Node(value);
+    }
 }
 
 class Node {
@@ -10,3 +20,11 @@ class Node {
         this.next = null;
     }
 }
+
+let node1 = new Node(2)
+let node2 = new Node(5)
+node1.next = node2
+
+let list = new LinkedList(node1);
+
+console.log(list);
